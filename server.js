5663f225cb2d87e2cdee47e8e7bb9ca325613a08
Shehroze4543 <86188771+Shehroze4543@users.noreply.github.com>
2023-08-08 22:39:06 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ app.get("/greetings/:name", (req, res) => {
   res.send(`Howdy!! ` + `${req.params.name}` + `. Its so good to see you.`);
 });
 
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+  const bill = [req.params.total * (req.params.tipPercentage / 100) + []];
+  res.send(...bill);
+});
+
 app.listen(3010, function () {
   console.log("Listening on Port 3010");
 });
